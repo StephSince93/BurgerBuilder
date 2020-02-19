886821611b87b7199dd46e8976e8f36b5f0e7222
Stem Dev <stephen@stemsoftware.com>
2020-02-19 09:58:39 -0600

    Updates to app. Added routing, http calls, updated code, and fixed bugs

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
@@ -50,7 +50,7 @@ class BurgerIngredient extends Component {
 }
 
 
-BurgerIngredient.PropTypes = {
-        type: PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+        type: propTypes.string.isRequired
 };
 export default BurgerIngredient;
